2f9738ee6a228c804d7d34926036b6f8c6106839
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2025-01-25 14:18:47 +0700

    lint

--- src/Vsrstudio/TopupRank/Forms/AdminForm.php
+++ src/Vsrstudio/TopupRank/Forms/AdminForm.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace VsrStudio\TopupRank\Forms;
+namespace Vsrstudio\TopupRank\Forms;
 
 use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\player\Player;
-use VsrStudio\TopupRank\Main;
+use Vsrstudio\TopupRank\Main;
 
 class AdminForm {
 
